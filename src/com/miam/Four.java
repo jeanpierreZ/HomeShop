127f2e0b493a5d5a9be38db9b1003c627b103f70
jpz <jeanpierrezingraff@gmail.com>
2018-11-27 10:33:18 +0100

    Create Class Main in com.Miam to try an exception Add IllegalArgumentException in Four

--- src/com/miam/Four.java
+++ src/com/miam/Four.java
@@ -7,6 +7,14 @@ public class Four {
     private Resistance resistance;
     private Soufflerie soufflerie;
 
+    public void cuire(int temperature, int duree) {
+        if (temperature < 40 || temperature > 250)
+            throw new IllegalArgumentException("La température du four doit être comprise en 40 et 250 degrés");
+        if (duree <= 0)
+            throw new IllegalArgumentException("La durée doit être positive");
+        System.out.println("Je cuis un plat à " + temperature + " degrés, pendant " + duree + " minutes.");
+    }
+
     public void cuire(Aliment aliment) {
         System.out.println("Je cuis un aliment : " + aliment.nom);
         System.out.println("avec ma capacité de " + capacite + " litres");
---
+++ src/com/miam/Main.java
@@ -0,0 +1,8 @@
+package com.miam;
+
+public class Main {
+    public static void main(String[] args) {
+        Four four = new Four();
+        four.cuire(30, 15);
+    }
+}
